cda7ddaf2236f40157ae44e4748273d6517a60a4
Mr.Rene <venelrene@gmail.com>
2015-10-10 16:35:18 -0400

    Revert "this final migration should work"
    This reverts commit 710b9f47185d25b71eb58be51f903e3c875a59a8.

--- db/migrate/20151010193328_add_price_to_products.rb
+++ db/migrate/20151010193328_add_price_to_products.rb
@@ -1,7 +1,5 @@
 class AddPriceToProducts < ActiveRecord::Migration
   def change
     add_column :products, :price, :decimal, precision: 12, scale: 3
-    add_column :products, :active, :boolean
   end
 end
-
