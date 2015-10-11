d7e0cb9706a4f5cd03121eaa79e4d9a665bd0223
Mr.Rene <venelrene@gmail.com>
2015-10-10 20:43:34 -0400

    removed the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
-Product.create! id: 1, product_photo: Faker::Avatar.image("my-own-slug", "220x220"), description: "Banana Hat", price: 14.99, active: true
-Product.create! id: 2, product_photo: Faker::Avatar.image("my-own-slug", "220x220"), description: "Apple Hat", price: 10.00, active: true
-Product.create! id: 3, product_photo: Faker::Avatar.image("my-own-slug", "220x220"), description: "Carton of Strawberries HAt", price: 14.99,
-    active:("my-own-slug", "220x220"),
-    true
+Product.delete_all
+Product.create! id: 101, product_photo: Faker::Avatar.image("my-own-slug", "220x220"), description: "Banana Hat",
+                price: 14.99, active: true
+Product.create! id: 201, product_photo: Faker::Avatar.image("my-own-slug", "220x220"), description: "Apple Hat",
+                price: 10.00, active: true
+Product.create! id: 301, product_photo: Faker::Avatar.image("my-own-slug", "220x220"), description: "Carton of
+Strawberries HAt", price: 14.99, active: true
 
 OrderStatus.delete_all
-OrderStatus.create! id: 1, name: "In Progress"
-OrderStatus.create! id: 2, name: "Placed"
-OrderStatus.create! id: 3, name: "Shipped"
-OrderStatus.create! id: 4, name: "Cancelled"
+OrderStatus.create! id: 101, name: "In Progress"
+OrderStatus.create! id: 201, name: "Placed"
+OrderStatus.create! id: 301, name: "Shipped"
+OrderStatus.create! id: 401, name: "Cancelled"
