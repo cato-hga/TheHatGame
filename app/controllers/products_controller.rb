e2f617f7bbc16de03874f2cbe2602c49e6c30385
Mr.Rene <venelrene@gmail.com>
2015-10-13 19:29:29 -0400

    Done editing the product controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,7 +5,7 @@ class ProductsController < ApplicationController
   # GET /products.json
   def index
     @products = Product.all
-
+    @order_item = current_order.order_items.new
     @instagram = ::Instagram.user_recent_media(496631234, {:count => 3})
 
   end
