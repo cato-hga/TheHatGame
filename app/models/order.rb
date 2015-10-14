b3a9eb38e069c6419d3a5eaf369dc87faae2c513
Mr.Rene <venelrene@gmail.com>
2015-10-13 22:19:24 -0400

    html for the cart view page render some notices

--- app/models/order.rb
+++ app/models/order.rb
@@ -20,7 +20,7 @@ class Order < ActiveRecord::Base
 
 
   def subtotal
-    order_items.collect { |oi| oi.valid? (oi.quantity * oi.unit_price) : 0 }. sum
+    order_items.collect { |oi| oi.valid? ? (oi.quantity * oi.unit_price) : 0 }.sum
   end
 
   private
