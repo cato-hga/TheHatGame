cc566a68d82b0996290f8d416658a7c304275d3f
JohnCCato <hga813@gmail.com>
2015-10-18 20:34:22 -0400

    Implemented Pagination Using Kaminari

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all
+    @products = Product.page(params[:page]).per(1)
 
     @instagram = ::Instagram.user_recent_media(496631234, {:count => 3})
 
