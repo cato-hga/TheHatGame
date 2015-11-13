ef04a21192f2eb7c5c837fb6a75fa1b87bb4308e
Mr.Rene <venelrene@gmail.com>
2015-11-13 08:48:30 -0500

    having a paras issue

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,5 +13,8 @@ class SessionsController < ApplicationController
   # def instagram_params
   #   params.require(:instagram_params).permit(:price, :instagram_photo, :description, :photo, :instagram_number, :instagram_photo, :instagram_photo_cache, {instagram_photo: []} )
   # end
-
+  def product_params
+    params.require(:product).permit(:price, :description, :photo, :product_number, :product_photo, :product_photo_cache,
+                                    :remote_product_photo_url, :remove_product_photo, {product_photo: []}, :page )
+  end
 end
