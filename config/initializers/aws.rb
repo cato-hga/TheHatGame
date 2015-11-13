65df70ff379476e3e55cb0f81666ee9b9967a934
Mr.Rene <venelrene@gmail.com>
2015-11-13 08:02:12 -0500

    removed the :provider key
    Still having issues with heroku

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,22 +1,37 @@
-CarrierWave.configure do |config|
-  #
-  # config.storage    = :aws
-  # config.aws_bucket = ENV.fetch('S3_BUCKET')
-  # config.aws_acl    = 'public-read'
-  # config.asset_host = 'https://s3.amazonaws.com'
+# CarrierWave.configure do |config|
+#   #
+#   # config.storage    = :aws
+#   # config.aws_bucket = ENV.fetch('S3_BUCKET')
+#   # config.aws_acl    = 'public-read'
+#   # config.asset_host = 'https://s3.amazonaws.com'
+#
+#
+#   config.storage    = :aws
+#   config.aws_bucket = 'thehatgame'
+#   config.aws_acl    = :public_read
+#   # config.asset_host = 'thehatgame.s3-website-us-west-2.amazonaws.com'
+#   config.aws_authenticated_url_expiration = 60 * 60 * 24 * 365
+#
+#
+#   config.aws_credentials = {
+#       :access_key_id        => ENV['SECRET_KEY'],
+#       :secret_access_key    => ENV['SECRET_ACCESS_KEY'],
+#       :region                 => ENV['S3_REGION']
+#   }
+# end
 
+CarrierWave.configure do |config|
 
   config.storage    = :aws
   config.aws_bucket = 'thehatgame'
   config.aws_acl    = :public_read
-  # config.asset_host = 'thehatgame.s3-website-us-west-2.amazonaws.com'
   config.aws_authenticated_url_expiration = 60 * 60 * 24 * 365
 
 
   config.aws_credentials = {
-      :provider                 => 'AWS',
-      :access_key_id        => ENV['SECRET_KEY'],
-      :secret_access_key    => ENV['SECRET_ACCESS_KEY'],
-      :region                 => ENV['S3_REGION']
+      access_key_id:      ENV.fetch('SECRET_KEY'),
+      secret_access_key:  ENV.fetch('SECRET_ACCESS_KEY'),
+      region:             ENV.fetch('S3_REGION')
   }
+
 end
